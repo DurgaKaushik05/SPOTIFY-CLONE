0eb3bad096c3b8498bd4d8caee8cf1d8a915b32f
DurgaKaushik05 <durgakaushik005@gmail.com>
2026-08-15 23:01:27 +0530

    file updated

--- script.js
+++ script.js
@@ -9,42 +9,48 @@ const progressBar = document.querySelector(".progress-bar");
 const songName = document.querySelector(".song-name");
 const singerName = document.querySelector(".singer-name");
 
+// Add your own legally usable audio files and update the "file" paths.
 const songs = [
     {
-        name: "Beautiful Dream",
-        singer: "Diego Nava",
-        file: "./songs/mixkit-beautiful-dream-493.mp3"
+        name: "Demo Song 1",
+        singer: "Demo Artist",
+        file: ""
     },
     {
-        name: "Hazy After Hours",
-        singer: "Alejandro Magaña",
-        file: "./songs/mixkit-hazy-after-hours-132.mp3"
+        name: "Demo Song 2",
+        singer: "Demo Artist",
+        file: ""
     },
     {
-        name: "Hip Hop 02",
-        singer: "Lily J",
-        file: "./songs/mixkit-hip-hop-02-738.mp3"
+        name: "Demo Song 3",
+        singer: "Demo Artist",
+        file: ""
     }
 ];
-
 let currentSong = 0;
-const audio = new Audio();
 
-// Load song
+const audio = new Audio();
 function loadSong(index) {
     currentSong = index;
-
-    audio.src = songs[index].file;
 
     songName.innerText = songs[index].name;
     singerName.innerText = songs[index].singer;
 
     currTime.innerText = "00:00";
+    totalTime.innerText = "00:00";
     progressBar.value = 0;
+
+    if (songs[index].file) {
+        audio.src = songs[index].file;
+    }
 }
 
-// Play / Pause
 playBtn.addEventListener("click", function () {
+
+    if (!songs[currentSong].file) {
+        alert("Please add a valid audio file to play the song.");
+        return;
+    }
 
     if (audio.paused) {
         audio.play();
@@ -53,7 +59,6 @@ playBtn.addEventListener("click", function () {
     }
 });
 
-// Current time
 audio.addEventListener("timeupdate", function () {
 
     let current = audio.currentTime;
@@ -66,7 +71,6 @@ audio.addEventListener("timeupdate", function () {
     }
 
     currTime.innerText = `${minutes}:${seconds}`;
-
     if (audio.duration) {
         progressBar.value =
             (audio.currentTime / audio.duration) * 100;
@@ -74,19 +78,16 @@ audio.addEventListener("timeupdate", function () {
 });
 
 audio.addEventListener("loadedmetadata", function () {
-
     let minutes = Math.floor(audio.duration / 60);
     let seconds = Math.floor(audio.duration % 60);
 
     if (seconds < 10) {
         seconds = "0" + seconds;
     }
-
     totalTime.innerText = `${minutes}:${seconds}`;
 });
 
 progressBar.addEventListener("input", function () {
-
     if (audio.duration) {
         audio.currentTime =
             (progressBar.value / 100) * audio.duration;
@@ -94,29 +95,28 @@ progressBar.addEventListener("input", function () {
 });
 
 nextBtn.addEventListener("click", function () {
-
     currentSong++;
-
     if (currentSong >= songs.length) {
         currentSong = 0;
     }
 
     loadSong(currentSong);
-    audio.play();
+    if (songs[currentSong].file) {
+        audio.play();
+    }
 });
 
 previousBtn.addEventListener("click", function () {
-
     currentSong--;
-
     if (currentSong < 0) {
         currentSong = songs.length - 1;
     }
 
     loadSong(currentSong);
-    audio.play();
+    if (songs[currentSong].file) {
+        audio.play();
+    }
 });
-
 audio.addEventListener("ended", function () {
 
     currentSong++;
@@ -124,8 +124,12 @@ audio.addEventListener("ended", function () {
     if (currentSong >= songs.length) {
         currentSong = 0;
     }
+
     loadSong(currentSong);
-    audio.play();
+    if (songs[currentSong].file) {
+        audio.play();
+    }
 });
 
+// Load first song information without automatically playing it
 loadSong(0);
